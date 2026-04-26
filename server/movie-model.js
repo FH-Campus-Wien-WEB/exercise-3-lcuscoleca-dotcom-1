0efd89f72648da85563ac1c6644ca0d4d57cf041
lcuscoleca-dotcom <lcuscoleca@gmail.com>
2026-04-26 17:49:10 +0200

    minor changes

--- server/movie-model.js
+++ server/movie-model.js
@@ -69,23 +69,19 @@ const movies = {
       Metascore: 82,
       imdbRating: 8.8
     },
-
-    tt0109830: {
-      imdbID:"tt0109830",
+    tt0076759: {
+      imdbID:"tt0076759",
       Title: "Star Wars: Episode IV - A New Hope ",
-      Released: "1994-7-6",
-      Runtime: 142,
-      Genres: ["Drama", "Romance"],
-      Directors: ["Robert Zemeckis"],
-      Writers: ["Winston Groom, Eric Roth"],
-      Actors: ["Tom Hanks, Robin Wright, Gary Sinise"],
-      Plot: "The history of the United States from the 1950s to the '70s unfolds from the perspective of an Alabama man with an IQ of 75, who yearns to be reunited with his childhood sweetheart",
-      Poster: "https://m.media-amazon.com/images/M/MV5BNDYwNzVjMTItZmU5YS00YjQ5LTljYjgtMjY2NDVmYWMyNWFmXkEyXkFqcGc@._V1_QL75_UY562_CR4,0,380,562_.jpg",
+      Released: "1977-5-25",
+      Runtime: 121,
+      Genres: ["Action", "Adventure", "Fantasy"],
+      Directors: ["George Lucas"],
+      Writers: ["George Lucas"],
+      Actors: ["Mark Hamill, Harrison Ford, Carrie Fisher"],
+      Plot: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth ...",
+      Poster: "https://m.media-amazon.com/images/M/MV5BOGUwMDk0Y2MtNjBlNi00NmRiLTk2MWYtMGMyMDlhYmI4ZDBjXkEyXkFqcGc@._V1_SX300.jpg",
       Metascore: 82,
       imdbRating: 8.8
-
-    {"Title":"Star Wars: Episode IV - A New Hope","Year":"1977","Rated":"PG","Released":"25 May 1977","Runtime":"121 min","Genre":"Action, Adventure, Fantasy","Director":"George Lucas","Writer":"George Lucas","Actors":"Mark Hamill, Harrison Ford, Carrie Fisher","Plot":"Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth ...","Language":"English","Country":"United States","Awards":"Won 6 Oscars. 70 wins & 31 nominations total","Poster":"https://m.media-amazon.com/images/M/MV5BOGUwMDk0Y2MtNjBlNi00NmRiLTk2MWYtMGMyMDlhYmI4ZDBjXkEyXkFqcGc@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"8.6/10"},{"Source":"Rotten Tomatoes","Value":"94%"},{"Source":"Metacritic","Value":"90/100"}],"Metascore":"90","imdbRating":"8.6","imdbVotes":"1,555,851","imdbID":"tt0076759","Type":"movie","DVD":"N/A","BoxOffice":"$460,998,507","Production":"N/A","Website":"N/A","Response":"True"}
-
-
-   };
+   }
+};
    module.exports = movies;
