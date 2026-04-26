ae5ecf2f9884bd2f8edbcbcafc37cd3a72369379
lcuscoleca-dotcom <lcuscoleca@gmail.com>
2026-04-26 18:00:09 +0200

    finished

--- server/files/index.js
+++ server/files/index.js
@@ -204,26 +204,3 @@ function loadMovies(genre) {
   xhr.open("GET", url);
   xhr.send();
 }
-/*window.onload = function () {
-const xhr = new XMLHttpRequest();
-xhr.onload = function () {
-const bodyElement = document.querySelector("body");
-if (xhr.status == 200) {
-const movies = JSON.parse(xhr.responseText);
-for (const movie of movies) {
-/* Task 1.3. Add your code from exercise 1 here 
-   and include a non-functional 'Edit' button
-   to pass this test 
-}
-} else {
-bodyElement.append(
-"Daten konnten nicht geladen werden, Status " +
-  xhr.status +
-  " - " +
-  xhr.statusText
-);
-}
-};
-xhr.open("GET", "/movies");
-xhr.send();
-};*/
